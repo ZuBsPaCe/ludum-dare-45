3fed49b54ff6436b43448f5ca7042375147a8025
ZuBsPaCe <kurt.rudin@gmx.net>
2019-10-05 12:55:39 +0200

    Player Idle and Left/Right Animations

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -4,6 +4,11 @@ namespace zs.Assets.Scripts
 {
     public static class Extensions
     {
+        public static Vector3 with_x(this Vector3 v, float x)
+        {
+            return new Vector3(x, v.y, v.z);
+        }
+
         public static Vector3 with_z(this Vector3 v, float z)
         {
             return new Vector3(v.x, v.y, z);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,11 +9,18 @@ namespace zs.Assets.Scripts
         [SerializeField]
         private float _speed = 3f;
 
+        [SerializeField]
+        private Transform _spritesLeft = null;
+
         #endregion Serializable Fields
 
         #region Private Vars
 
         private Rigidbody2D _rigidbody = null;
+        private Animator _animator = null;
+
+        private bool _walkLeft = false;
+        private bool _walkRight = false;
 
         #endregion Private Vars
 
@@ -28,8 +35,11 @@ namespace zs.Assets.Scripts
         void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
 
             Debug.Assert(_rigidbody);
+            Debug.Assert(_animator);
+            Debug.Assert(_spritesLeft);
         }
 
         void Start()
@@ -62,6 +72,34 @@ namespace zs.Assets.Scripts
             {
                 velocity.y -= 1;
             }
+
+            bool walkLeft = false;
+            bool walkRight = false;
+
+
+            if (velocity.x > 0)
+            {
+                walkRight = true;
+                _spritesLeft.localScale = _spritesLeft.localScale.with_x(-1);
+            }
+            else if (velocity.x < 0)
+            {
+                walkLeft = true;
+                _spritesLeft.localScale = _spritesLeft.localScale.with_x(1);
+            }
+
+            if (_walkRight != walkRight)
+            {
+                _walkRight = walkRight;
+                _animator.SetBool("WalkRight", _walkRight);
+            }
+
+            if (_walkLeft != walkLeft)
+            {
+                _walkLeft = walkLeft;
+                _animator.SetBool("WalkLeft", _walkLeft);
+            }
+
 
             velocity = velocity.normalized * _speed;
 
