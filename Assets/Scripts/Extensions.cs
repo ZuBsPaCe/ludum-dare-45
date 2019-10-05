efa88ef719aa0e127e5fc11ba72cc974e3ae8337
ZuBsPaCe <kurt.rudin@gmx.net>
2019-10-05 10:43:54 +0200

    Fence Sprite and FollowCamera

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace zs.Assets.Scripts
+{
+    public static class Extensions
+    {
+        public static Vector3 with_z(this Vector3 v, float z)
+        {
+            return new Vector3(v.x, v.y, z);
+        }
+    }
+}
---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace zs.Assets.Scripts
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        #region Serializable Fields
+
+        [SerializeField]
+        private Transform _followObject = null;
+
+        #endregion Serializable Fields
+
+        #region Private Vars
+        #endregion Private Vars
+
+        #region Public Vars
+        #endregion Public Vars
+
+        #region Public Methods
+
+        public Transform FollowObject
+        {
+            get { return _followObject; }
+            set { _followObject = value; }
+        }
+
+        #endregion Public Methods
+
+        #region MonoBehaviour
+	
+        void Awake()
+        {
+            Debug.Assert(_followObject);
+        }
+
+        void Start()
+        {
+        }
+	
+        void LateUpdate()
+        {
+            if (_followObject == null)
+            {
+                return;
+            }
+
+            transform.position = _followObject.position.with_z(transform.position.z);
+        }
+
+        #endregion MonoBehaviour
+
+        #region Private Methods
+        #endregion Private Methods
+    }
+}
